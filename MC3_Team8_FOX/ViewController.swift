ec2b790a77b2c941a1543a0075a1c99d7cf622e8
Juny0110 <wnsdyds403@naver.com>
2022-07-14 12:06:47 +0900

    Feat✨: Team8 프로젝트 생성

---
+++ MC3_Team8_FOX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MC3_Team8_FOX
+//
+//  Created by 지준용 on 2022/07/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
