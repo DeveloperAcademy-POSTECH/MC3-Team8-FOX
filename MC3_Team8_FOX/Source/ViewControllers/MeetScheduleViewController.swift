7d09637f2259a08828cf91bd4bda11647d22a245
lululll <yeekim182@gmail.com>
2022-07-20 09:23:52 +0900

    Feat✨: add MeetScheduleView Controller and storyboard

---
+++ MC3_Team8_FOX/Source/ViewControllers/MeetScheduleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MeetScheduleView.swift
+//  MC3_Team8_FOX
+//
+//  Created by yeekim on 2022/07/20.
+//
+
+import UIKit
+
+class MeetScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
