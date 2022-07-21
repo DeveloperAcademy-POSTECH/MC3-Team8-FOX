2fccf1832d8df6463ec14cf99946dc2350eaa1a3
Juny0110 <wnsdyds403@naver.com>
2022-07-21 10:34:06 +0900

    Feat✨: Label 및 Button 변수명 추가 (#5)
    - Label 및 Button 변수명 추가

--- MC3_Team8_FOX/Source/ViewControllers/AcceptRequestViewController.swift
+++ MC3_Team8_FOX/Source/ViewControllers/AcceptRequestViewController.swift
@@ -9,8 +9,27 @@ import UIKit
 
 class AcceptRequestViewController: UIViewController {
 
+    @IBOutlet weak var meetTitle: UILabel!
+    @IBOutlet weak var startTime: UILabel!
+    @IBOutlet weak var endTime: UILabel!
+    @IBOutlet weak var destination: UILabel!
+    @IBOutlet weak var activity: UILabel!
+    @IBOutlet weak var pickUpLocation: UITextField!
+    @IBOutlet weak var sendingNotice: UITableView!
+
+    @IBOutlet weak var acceptingBtn: UIButton!
+    @IBOutlet weak var rejectingBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-}
 
+
+    @IBAction func tapAcceptingBtn(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapRejectingBtn(_ sender: UIButton) {
+
+    }
+}
