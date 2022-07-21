57f4faf966c26beee94e8e43335bbc2cd287ad1b
lululll <yeekim182@gmail.com>
2022-07-21 16:38:02 +0900

    Style🧩: 주석 정리

--- MC3_Team8_FOX/Source/ViewControllers/MeetScheduleViewController.swift
+++ MC3_Team8_FOX/Source/ViewControllers/MeetScheduleViewController.swift
@@ -10,16 +10,8 @@ import UIKit
 class MeetScheduleViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
-    
-    
-    let leftTimes: [String] = ["07.10", "07.12", "07.15", "07.19"]
-    let middleTimes: [String] = ["13:00", "14:00", "13:30", "16:00"]
-//    let meetTitle: [String] = ["할머니 생신", "생일파티", "뽁스 생일", "놀이동산"]
-    
-    
     let appointmentCount = LoadData().appointment.count
     let data = LoadData().appointment
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
