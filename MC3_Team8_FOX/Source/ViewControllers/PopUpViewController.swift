72d7196880e2bd98a9250b75e11e6fdbc23896f3
sunchoi22 <sunchoi22@pos.idserve.net>
2022-07-22 16:43:27 +0900

    Feat✨:메인 뷰로 이동하면서 팝업 창 뜸 (#35)

---
+++ MC3_Team8_FOX/Source/ViewControllers/PopUpViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpViewController.swift
+//  MC3_Team8_FOX
+//
+//  Created by 최성희 on 2022/07/22.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
