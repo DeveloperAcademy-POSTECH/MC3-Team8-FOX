8eef26e30239bd75416fafccebc15404fc2a62c6
Juny0110 <wnsdyds403@naver.com>
2022-07-23 20:43:00 +0900

    Feat✨: Add CheckBox & Load data to TableView (#25)
    - 체크박스 추가 ( 클릭 x )
    - UITableView에 데이터 불러오기 가능. -> 추후 JSON데이터 추가 필요.

---
+++ MC3_Team8_FOX/Source/Component/CheckBoxDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  CheckBoxDelegate.swift
+//  MC3_Team8_FOX
+//
+//  Created by 지준용 on 2022/07/22.
+//
+
+import UIKit
+
+protocol CheckBoxDelegate: AnyObject {
+    func checkBoxHighlight(checkBox: CheckBoxView)
+}
