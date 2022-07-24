949f474551e66c26040fc63aaf89ea6710b97f4d
Juny0110 <wnsdyds403@naver.com>
2022-07-25 02:39:55 +0900

    Style🧩:	불필요 Enter삭제, 출처 기입 ( #17  )
    - 불필요 Enter삭제
    - Swipe Segmented Control 출처 기입https://www.youtube.com/watch?v=SbeYBZHGvxw

--- MC3_Team8_FOX/Source/ViewControllers/MeetScheduleViewController.swift
+++ MC3_Team8_FOX/Source/ViewControllers/MeetScheduleViewController.swift
@@ -8,36 +8,75 @@
 import UIKit
 
 class MeetScheduleViewController: UIViewController {
-    
+
     @IBOutlet weak var meetScheduleTableView: UIView!
     @IBOutlet weak var recentRequestView: UIView!
     @IBOutlet weak var tableView: UITableView!
-    
+
+    @IBOutlet weak var segment: UISegmentedControl!
+    var views: [UIView] = []
+    var index = 0
+
     let leftTimes: [String] = ["07.10", "07.12", "07.15", "07.19"]
     let middleTimes: [String] = ["13:00", "14:00", "13:30", "16:00"]
     let meetTitle: [String] = ["할머니 생신", "생일파티", "뽁스 생일", "놀이동산"]
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        views = [meetScheduleTableView, recentRequestView]
+
+        segment.selectedSegmentIndex = index
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture(gesture:)))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleGesture(gesture:)))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        
+        self.view.bringSubviewToFront(meetScheduleTableView)
+        
         self.tableView?.delegate = self
         self.tableView?.dataSource = self
-//        switchView()
+        
     }
-    
-    @IBAction func switchView(_ sender: UISegmentedControl) {
-        if sender.selectedSegmentIndex == 0 {
-            self.meetScheduleTableView.alpha = 1.0
-            self.recentRequestView.alpha = 0.0
-        } else if sender.selectedSegmentIndex == 1 {
-            self.meetScheduleTableView.alpha = 0.0
-            self.recentRequestView.alpha = 1.0
+
+
+    @objc func handleGesture(gesture: UISwipeGestureRecognizer) {
+        if index >= 0 && index < views.count {
+            if gesture.direction == UISwipeGestureRecognizer.Direction.right {
+                if index != 0 {
+                    index -= 1
+                    self.view.bringSubviewToFront(views[index])
+                    segment.selectedSegmentIndex = index
+                }
+            }
+
+            if gesture.direction == UISwipeGestureRecognizer.Direction.left {
+                if index != views.count - 1 {
+                    index += 1
+                    segment.selectedSegmentIndex = index
+                    self.view.bringSubviewToFront(views[index])
+                }
+            }
         }
     }
     
+    @IBAction func switchView(_ sender: UISegmentedControl) {
+        self.view.bringSubviewToFront(views[sender.selectedSegmentIndex])
+        index = segment.selectedSegmentIndex
+//        if sender.selectedSegmentIndex == 0 {
+//            self.meetScheduleTableView.alpha = 1.0
+//            self.recentRequestView.alpha = 0.0
+//        } else if sender.selectedSegmentIndex == 1 {
+//            self.meetScheduleTableView.alpha = 0.0
+//            self.recentRequestView.alpha = 1.0
+//        }
+    }
+    
 }
-
-
-
 
 extension MeetScheduleViewController: UITableViewDelegate {
     
@@ -59,4 +98,4 @@ extension MeetScheduleViewController: UITableViewDataSource {
     }
 }
 
-
+//출처: https://www.youtube.com/watch?v=SbeYBZHGvxw
