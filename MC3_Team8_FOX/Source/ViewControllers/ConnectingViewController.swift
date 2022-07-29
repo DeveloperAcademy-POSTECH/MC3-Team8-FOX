9b093e1e2b2b8030f175c6fbf3a8606b8cebe24e
sherry <yeahaluu@gmail.com>
2022-07-29 10:45:03 +0900

    Wip⚙: modify connecting view layout

--- MC3_Team8_FOX/Source/ViewControllers/ConnectingViewController.swift
+++ MC3_Team8_FOX/Source/ViewControllers/ConnectingViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class ConnectingViewController: UIViewController {
-
+    
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var inputCodeTextField: UITextField!
+    @IBOutlet weak var inputCodeButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputCodeTextField.layer.cornerRadius = 12
+        inputCodeButton.layer.cornerRadius = 12
     }
+    
 }
-
