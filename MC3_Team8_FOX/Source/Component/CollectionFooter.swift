a7e0eadaa80322c756cdd6bcd03c90e236e0faab
lululll <yeekim182@gmail.com>
2022-07-31 18:27:40 +0900

    test

---
+++ MC3_Team8_FOX/Source/Component/CollectionFooter.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionFooter.swift
+//  MC3_Team8_FOX
+//
+//  Created by yeekim on 2022/07/31.
+//
+
+import UIKit
+
+class CollectionFooter: UICollectionReusableView {
+        
+}
---
+++ MC3_Team8_FOX/Source/Component/CollectionHeader.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionHeader.swift
+//  MC3_Team8_FOX
+//
+//  Created by yeekim on 2022/07/31.
+//
+
+import UIKit
+
+class CollectionHeader: UICollectionReusableView {
+        
+}
