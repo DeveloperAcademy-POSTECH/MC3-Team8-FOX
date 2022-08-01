8396332c57e596fa0bd2bcb1c3da2d9bdf2092e9
Juny0110 <wnsdyds403@naver.com>
2022-08-01 21:38:47 +0900

    Fix🐞: ConnectingView storyboard가 나타나지 않는 버그 해결

--- MC3_Team8_FOX/Source/ViewControllers/ConnectingViewController.swift
+++ MC3_Team8_FOX/Source/ViewControllers/ConnectingViewController.swift
@@ -7,10 +7,22 @@
 
 import UIKit
 
-class ConnectingViewController: UIViewController {
+class ConnectingViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var appNameLabel: UILabel!
+    @IBOutlet weak var inputCodeTextField: UITextField!
+    @IBOutlet weak var inputCodeButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        inputCodeTextField.layer.cornerRadius = 12
+        inputCodeButton.layer.cornerRadius = 12
+        
+        inputCodeTextField.delegate = self
+        inputCodeButton.isUserInteractionEnabled = false
+        inputCodeTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        
     }
     
     @objc func textFieldDidChange(sender: UITextField) {
